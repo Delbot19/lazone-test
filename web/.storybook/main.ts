797d9cc36b71a46e29bdeecbd69c67beba1a2b37
Delbot19 <delbotoussou00@gmail.com>
2024-10-02 13:48:55 +0100

    delete stories folder

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@chakra-ui/storybook-addon",
   ],
   framework: {
     name: "@storybook/react-webpack5",
