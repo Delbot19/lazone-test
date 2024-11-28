80b561be279ad282e586e05b4467bd3e006c5baf
Delbot19 <delbotoussou00@gmail.com>
2024-11-28 16:13:04 +0100

    utiliser DB_NAME depuis .env pour la config BD

--- api/src/config/ormconfig.ts
+++ api/src/config/ormconfig.ts
@@ -1,8 +1,18 @@
 import { DataSource } from "typeorm";
 import { User } from "../entities/users";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbName = process.env.DB_NAME;
+
+if (!dbName) {
+  throw new Error("La variable d'environnement DB_NAME n'est pas définie !");
+}
+
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "test.sqlite",
+  database: dbName,
   synchronize: true,
   logging: true,
   entities: [User],
